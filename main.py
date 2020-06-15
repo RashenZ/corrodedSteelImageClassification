f25725052b9af06b7bea467d464b216d4d6fd62c
David XU <david.xu@epitech.eu>
2020-06-15 06:18:28 +0200

    retrive files name from folder trainin set

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from os import listdir
 from os.path import isfile, join
 
 def retrieve_files():
-    trainingPath = "TrainingSet/"
-    files = [f for f in listdir(trainingPath) if isfile(join(trainingPath, f))]
-    print(files)
-
+    path = "TrainingSet/label0"
+    filename_labelZero = [f for f in listdir(path) if isfile(join(path, f))]
+    path = "TrainingSet/label1"
+    filename_labelOne = [f for f in listdir(path) if isfile(join(path, f))]
+    path = "TrainingSet/label2"
+    filename_labelTwo = [f for f in listdir(path) if isfile(join(path, f))]
+    
+    pass
+    
 if __name__ == "__main__":
     retrieve_files()
     pass
